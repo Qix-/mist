7c152ceac222ef531549a68c5ee7382fa55b3c62
Qix <i.am.qix@gmail.com>
2015-06-04 16:08:43 -0600

    colored errorneous output in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 success=true
+
+echo -en "\x1b[1;31m"
 node node_modules/coffee-script/bin/coffee -cbm --no-header -o bin src/mist.coffee || success=false
 node node_modules/pegjs/bin/pegjs --plugin pegjs-coffee-plugin -o speed src/mist-parser.pegcs bin/mist-parser.js || success=false
+echo -en "\x1b[0m"
 
 $success && echo "Built successfully"
