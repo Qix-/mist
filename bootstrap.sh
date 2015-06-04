57beaa4b9a61d4d9c7a5259354077600277dddc2
Qix <i.am.qix@gmail.com>
2015-06-04 09:50:40 -0600

    added commands to bootstrap file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-
+npm install
+node node_modules/coffee-script/bin/coffee -cbm --no-header -o bin src/mist.coffee
+node node_modules/pegjs/bin/pegjs --plugin pegjs-coffee-plugin -o speed -- src/mist-parser.pegcs bin/mist-parser.js
