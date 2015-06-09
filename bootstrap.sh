c4fa9289a6f8155db9c0e71f84765fb1b67c8921
Qix <i.am.qix@gmail.com>
2015-06-09 16:41:25 -0600

    added selective bootstrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,18 +3,27 @@ success=true
 
 npm install
 
-(cd ext/ninja && ./configure.py --bootstrap) || exit 1
+if [ -z "$1" ] || [ "$1" == "ninja" ]; then
+  shift
+  (cd ext/ninja && ./configure.py --bootstrap) || exit 1
+fi
 
-echo -en "\x1b[1;31m"
-node node_modules/coffee-script/bin/coffee -cbm --no-header -o bin src/mist.coffee || success=false
-node node_modules/coffee-script/bin/coffee -cbm --no-header -o bin src/mist-ninja-builder.coffee || success=false
-node node_modules/coffee-script/bin/coffee -cbm --no-header -o bin src/mist-globber.coffee || success=false
-node node_modules/pegjs/bin/pegjs --plugin pegjs-coffee-plugin -o speed src/mist-parser.pegcs bin/mist-parser.js || success=false
-echo -en "\x1b[0m"
+if [ -z "$1" ] || [ "$1" == "mist" ]; then
+  shift
+  echo -en "\x1b[1;31m"
+  node node_modules/coffee-script/bin/coffee -cbm --no-header -o bin src/mist.coffee || success=false
+  node node_modules/coffee-script/bin/coffee -cbm --no-header -o bin src/mist-ninja-builder.coffee || success=false
+  node node_modules/coffee-script/bin/coffee -cbm --no-header -o bin src/mist-globber.coffee || success=false
+  node node_modules/pegjs/bin/pegjs --plugin pegjs-coffee-plugin -o speed src/mist-parser.pegcs bin/mist-parser.js || success=false
+  echo -en "\x1b[0m"
+fi
+
+if [ -z "$1" ] || [ "$1" == "test" ]; then
+  shift
+  NINJA=`pwd`/ext/ninja/ninja node bin/mist.js
+  (cd test/hello && node ../../bin/mist.js) && test/hello/hello-mist || exit 1
+  (cd test/foreach && node ../../bin/mist.js) || exit 1
+fi
 
 $success && echo "Built successfully" || exit 1
 echo
-
-NINJA=`pwd`/ext/ninja/ninja node bin/mist.js
-(cd test/hello && node ../../bin/mist.js) && test/hello/hello-mist || exit 1
-(cd test/foreach && node ../../bin/mist.js) || exit 1
