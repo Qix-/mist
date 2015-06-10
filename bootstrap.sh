f995a18291fa750e7d3108779670e35c3a538ab7
Qix <i.am.qix@gmail.com>
2015-06-10 03:32:30 -0600

    improved bootstrap argument parsing logic

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,12 +4,11 @@ success=true
 npm install
 
 if [ -z "$1" ] || [ "$1" == "ninja" ]; then
-  shift
   (cd ext/ninja && ./configure.py --bootstrap) || exit 1
+  [ ! -z "$1" ] && shift && [ -z "$1" ] && exit
 fi
 
 if [ -z "$1" ] || [ "$1" == "mist" ]; then
-  shift
   echo -en "\x1b[1;31m"
   node node_modules/coffee-script/bin/coffee -cbm --no-header -o bin src/mist.coffee || exit 1
   node node_modules/coffee-script/bin/coffee -cbm --no-header -o bin src/mist-ninja-builder.coffee || exit 1
@@ -17,14 +16,15 @@ if [ -z "$1" ] || [ "$1" == "mist" ]; then
   node node_modules/coffee-script/bin/coffee -cbm --no-header -o bin src/array-utils.coffee || exit 1
   node node_modules/pegjs/bin/pegjs --plugin pegjs-coffee-plugin -o speed src/mist-parser.pegcs bin/mist-parser.js || exit 1
   echo -en "\x1b[0m"
+  [ ! -z "$1" ] && shift && [ -z "$1" ] && exit
 fi
 
 if [ -z "$1" ] || [ "$1" == "test" ]; then
-  shift
   NINJA=`pwd`/ext/ninja/ninja node bin/mist.js
   (cd test/hello && node ../../bin/mist.js) && test/hello/hello-mist || exit 1
   (cd test/foreach && node ../../bin/mist.js) || exit 1
   (cd test/group && node ../../bin/mist.js) || exit 1
+  [ ! -z "$1" ] && shift && [ -z "$1" ] && exit
 fi
 
 echo "Built successfully"
