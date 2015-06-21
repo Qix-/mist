6dc0f519ed3dd52ea890bc999d42cb832919346b
Qix <i.am.qix@gmail.com>
2015-06-21 01:10:38 -0600

    fixed bootstrapping process for npm (fixes #23)

---
+++ script/install-ninja.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var fs = require('fs');
+var spawn = require('child_process').spawn;
+
+try {
+  fs.mkdirSync('bin/ninja');
+} catch (e) {
+  // swallow
+}
+
+var proc = spawn('../../ext/ninja/configure.py', ['--bootstrap'], {
+  cwd: 'bin/ninja',
+  stdio: [null, process.stdout, process.stderr]
+});
+
+proc.on('close', process.exit);
