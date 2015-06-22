562dfc51749cc7e075a16c80980b8de1fe1af60e
Qix <i.am.qix@gmail.com>
2015-06-21 18:09:56 -0600

    fixed bootstrapping for global install

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,25 +11,26 @@ if [ -z "$1" ] || [ "$1" == "mist" ]; then
   function cofc {
     node node_modules/coffee-script/bin/coffee -cbm --no-header -o $*
   }
+  mkdir -p bin/tmp/bin
+  mkdir -p bin/tmp/lib
   # only the bare necessities to get mist to build itself
-  cofc bin src/mist.coffee || exit 1
-  cofc bin src/mist-build.coffee || exit 1
-  cofc lib src/lib/globber.coffee || exit 1
-  cofc lib src/lib/hasher.coffee || exit 1
-  cofc lib src/lib/mist-resolver.coffee || exit 1
-  cofc lib src/lib/mistfile.coffee || exit 1
-  cofc lib src/lib/utils.coffee || exit 1
-  cofc lib/renderer src/lib/renderer/ninja.coffee || exit 1
-  node node_modules/pegjs/bin/pegjs --plugin pegjs-coffee-plugin -o speed src/lib/mist-parser.pegcs lib/mist-parser.js || exit 1
+  cp package.json bin/tmp/package.json
+  cofc bin/tmp/bin src/mist.coffee || exit 1
+  cofc bin/tmp/bin src/mist-build.coffee || exit 1
+  cofc bin/tmp/lib src/lib/globber.coffee || exit 1
+  cofc bin/tmp/lib src/lib/hasher.coffee || exit 1
+  cofc bin/tmp/lib src/lib/mist-resolver.coffee || exit 1
+  cofc bin/tmp/lib src/lib/mistfile.coffee || exit 1
+  cofc bin/tmp/lib src/lib/utils.coffee || exit 1
+  cofc bin/tmp/lib/renderer src/lib/renderer/ninja.coffee || exit 1
+  node node_modules/pegjs/bin/pegjs --plugin pegjs-coffee-plugin -o speed src/lib/mist-parser.pegcs bin/tmp/lib/mist-parser.js || exit 1
   echo -en "\x1b[0m"
-  NINJA=`pwd`/bin/ninja/ninja node bin/mist.js || exit 1
+  NINJA=`pwd`/bin/ninja/ninja node bin/tmp/bin/mist.js || exit 1
+  rm -rf bin/tmp
   [ ! -z "$1" ] && shift && [ -z "$1" ] && exit
 fi
 
 if [ -z "$1" ] || [ "$1" == "test" ]; then
-  (cd test/hello && node ../../bin/mist.js) && test/hello/hello-mist || exit 1
-  (cd test/foreach && node ../../bin/mist.js) || exit 1
-  (cd test/group && node ../../bin/mist.js) || exit 1
   [ ! -z "$1" ] && shift && [ -z "$1" ] && exit
 fi
 
